223d44b0c198c89bad5fc2a1008044ce444ce4b5
Raghav <raghav.k911@gmail.com>
2024-09-15 15:10:24 +0530

    adding App.js

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Nav from './Navbar';
+import Header from './Header';
+import MainContent from './MainContent';
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Header />
+      <MainContent />
+    </div>
+
+  );
+}
+
+export default App;
